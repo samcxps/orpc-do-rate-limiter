40b698044f3a109b3c2b18f1abc33a357f51baec
samcxps <samsonc99@gmail.com>
2025-12-10 11:27:36 -0600

    chore: rm example

--- example/src/orpc.ts
+++
@@ -1,9 +0,0 @@
-import { os } from "@orpc/server";
-import type { CloudflareDurableRateLimiter } from "../../src/adapter";
-
-type ORPCContext = {
-  env: Env;
-  rateLimiter: CloudflareDurableRateLimiter;
-};
-
-export const pub = os.$context<ORPCContext>();
